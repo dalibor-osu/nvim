32d665e4d98d395dad8bb173b649f4d73ae3b680
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2024-04-15 17:20:27 +0200

    add keybinds

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,4 +3,7 @@ vim.keymap.set('n', '<leader>d', vim.lsp.buf.hover) -- why the fuck is this so s
 
 vim.cmd([[
     map <leader>e :lua vim.diagnostic.open_float(0, {scope = "line"})<CR>
+    map <leader>gt :bnext<CR>
+    map <leader>gT :bprev<CR>
+    map <leader>bd :bd<CR>
 ]])
